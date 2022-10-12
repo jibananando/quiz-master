22c69bc3c9eccb8e7132b3eefab3b8d83d56ead0
jibananando <jibananando.pramanik@gmail.com>
2022-10-12 23:22:46 +0600

    Added a chart in Statistics page

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         },
         {
           path: '/statistics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, XAxis, YAxis, Tooltip, BarChart, Legend, Bar, AreaChart, Area, PieChart, Pie } from 'recharts';
 
 
 const Statistics = () => {
+    const data = useLoaderData();
+    const quizes = data.data;
 
     return (
         <div>
-            <LineChart>
+            <BarChart
+                width={300}
+                height={300}
+                data={quizes}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-            </LineChart>
+                <Legend />
+                <Bar dataKey="total" fill="#82ca9d" />
+            </BarChart>
         </div>
     );
 };
